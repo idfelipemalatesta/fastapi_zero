bf33e8b62da7aff6dde2a5afcfa44b88feda12fb
Felipe Malatesta <idfelipemalatesta@gmail.com>
2026-03-06 18:09:06 -0300

    upload new files

---
+++ fastapi_zero/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserPublic(BaseModel):
+    username: str
+    email: EmailStr
+    id: int
+
+
+class UserDB(UserSchema):
+    id: int
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,47 @@
-from fastapi.testclient import TestClient
-
-from fastapi_zero.app import app
+from http import HTTPStatus
 
 
-def test_root_deve_retornar_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ola_mundo(client):
     response = client.get("/")
 
     assert response.json() == {"message": "Olá mundo!"}
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_create_user(client):
+    response = client.post(
+        "/users/",
+        json={"username": "alice", "email": "alice@example.com", "password": "secret"},
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "alice",
+        "email": "alice@example.com",
+        "id": 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get("/users/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {
+                "id": 1,
+                "username": "alice",
+                "email": "alice@example.com",
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        "/users/1",
+        json={"username": "bob", "email": "bob@example.com", "password": "secret"},
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json == {"username": "bob", "email": "bob@example.com", "id": 1}
